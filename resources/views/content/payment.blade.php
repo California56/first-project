af2354da71f0fbcd9cc346c720e232da4305322a
Daniel S <you@example.com>
2021-01-28 13:28:10 +0500

    visa

--- resources/views/content/payment.blade.php
+++ resources/views/content/payment.blade.php
@@ -31,5 +31,7 @@
             </p>
             <p> По всем вопросам обращайтесь по телефону <a href="https://wa.me/79088769117" > 8-908-876-91-17</a> или по эл.почте <a href="maito:voshod8574@mail.ru" >voshod8574@mail.ru</a>.
             </p>
+
+            <img class="logo-img" src="{{asset('images/visa-and-mastercard-logo-26.png')}}">
         </div>	
 @endsection
