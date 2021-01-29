a7a971e38cb447171eef59b3e5928489c866cfad
Daniel S <you@example.com>
2021-01-29 12:28:35 +0500

    delete b2b

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,16 +1,6 @@
 <footer class="footer sticky-bottom">
 	<div class="copyright">
 		© 2017-2021, Все права защищены. Интернет-магазин мухоморов "Сибирский мухомор".
-
-		<!-- Kassot.B2bito rating counter -->
-		<a href="https://kassot.com/b2bito/top/3022" target="_blank">
-		<script language="javascript">
-		var r = escape(document.referrer);
-		var rd = Math.random();
-		var pr = 'https://kassot.com/b2bito/top/counter';
-		document.write('<img src="'+pr+'?uid=3022&ref='+r+'&rd='+rd+'" border="0">');
-		</script></a>
-		<!-- /Kassot.B2bito rating counter -->
 	</div>
 
 	
