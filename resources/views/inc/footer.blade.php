929296a290ab14ab53b8d523d62f97aebb26f97c
Daniel S <you@example.com>
2021-01-29 12:14:06 +0500

    b2b rating

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,7 +2,19 @@
 	<div class="copyright">
 		© 2017-2021, Все права защищены. Интернет-магазин мухоморов "Сибирский мухомор".
 	</div>
+
+	<!-- Kassot.B2bito rating counter -->
+		<a href="https://kassot.com/b2bito/top/3022" target="_blank">
+		<script language="javascript">
+		var r = escape(document.referrer);
+		var rd = Math.random();
+		var pr = 'https://kassot.com/b2bito/top/counter';
+		document.write('<img src="'+pr+'?uid=3022&ref='+r+'&rd='+rd+'" border="0">');
+		</script></a>
+	<!-- /Kassot.B2bito rating counter -->
 </footer>
+
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="/js/app.js"></script>
